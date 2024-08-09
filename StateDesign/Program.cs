467b82c9dc26f1738e6d1b60d9c5ca886b238666
Pratik.Shrivastav <Pratik.Shrivastav@DELL1615>
2024-08-09 12:49:24 +0530

    Add project files.

---
+++ StateDesign/Program.cs
@@ -0,0 +1,38 @@
+﻿using StateDesign.Service;
+
+namespace StateDesign
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AtmMachine atmMachine = new AtmMachine();
+
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.WriteLine();
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+            atmMachine.EjectDebitCard();
+            atmMachine.InsertDebitCard();
+
+            Console.WriteLine();
+
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.WriteLine();
+
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+            atmMachine.InsertDebitCard();
+            atmMachine.EjectDebitCard();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+
+            Console.Read();
+        }
+    }
+}
---
+++ StateDesign/Service/AtmMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDesign.Service
+{
+    public class AtmMachine : IAtmState
+    {
+        public IAtmState AtmMachineState = null;
+        public AtmMachine()
+        {
+            AtmMachineState = new DebitCardNotInsertedSate();
+        }
+        public void InsertDebitCard()
+        {
+            AtmMachineState.InsertDebitCard();
+
+            if (AtmMachineState is DebitCardNotInsertedSate)
+            {
+                AtmMachineState = new DebitCardInsertedState();
+                Console.WriteLine($"ATM Machine internal state has been changed to : {AtmMachineState.GetType().Name}");
+            }
+        }
+        public void EjectDebitCard()
+        {
+            AtmMachineState.EjectDebitCard();
+
+            if (AtmMachineState is DebitCardInsertedState)
+            {
+                AtmMachineState = new DebitCardNotInsertedSate();
+                Console.WriteLine($"ATM Machine internal state has been Changed to : {AtmMachineState.GetType().Name}");
+            }
+        }
+        public void EnterPin()
+        {
+            AtmMachineState.EnterPin();
+        }
+        public void WithdrawMoney()
+        {
+            AtmMachineState.WithdrawMoney();
+        }
+    }
+}
+
